b9ea36ff72d0deb01d64e875ce1ffab9f0477f4f
hzphzp <hzp1104@mail.ustc.edu.cn>
2024-05-31 06:45:23 +0000

    prepare envs

--- src/tools/reload_qwen_vit.py
+++ src/tools/reload_qwen_vit.py
@@ -3,7 +3,7 @@ from transformers import AutoModelForCausalLM
 
 torch.manual_seed(1234)
 
-qwen_model_path = 'pretrained/Qwen-VL-Chat'
+qwen_model_path = 'pretrained/Qwen-VL-Chat/'
 save_path = 'pretrained/QwenViT/qwen_vit_G.pt'
 
 model = AutoModelForCausalLM.from_pretrained(qwen_model_path, device_map="cpu", trust_remote_code=True).eval()
