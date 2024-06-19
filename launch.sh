40b5a93130ea812a63309a3b093ed188e1d37b15
ethanphuang <ethanphuang@tencent.com>
2024-06-19 17:19:50 +0800

    update training

--- launch.sh
+++ launch.sh
@@ -1,8 +1,9 @@
 source /usr/local/Ascend/ascend-toolkit/set_env.sh
 export ASCEND_HOME_PATH=/usr/local/Ascend/ascend-toolkit/latest
 export PYTHONPATH=$PYTHONPATH:/usr/local/Ascend/ascend-toolkit/latest/python/site-packages:/usr/local/Ascend/ascend-toolkit/latest/opp/built-in/op_impl/ai_core/tbe:/usr/local/Ascend/ascend-toolkit/latest/opp/op_impl/built-in/ai_core/tbe
-
+wandb login 69672540eb30feaa4b6f38aa2b2aca504e0224ce
 
 # the input scripts path
 script_path=$1
-torchrun --nproc-per-node 8 --master-port 11111 $script_path
+# torchrun --nproc-per-node 8 --master-port 11111 $script_path
+python $script_path
--- scripts/train_seed_x_detokenizer.sh
+++ scripts/train_seed_x_detokenizer.sh
@@ -3,6 +3,8 @@ source /usr/local/Ascend/ascend-toolkit/set_env.sh
 export ASCEND_HOME_PATH=/usr/local/Ascend/ascend-toolkit/latest
 export PYTHONPATH=$PYTHONPATH:/usr/local/Ascend/ascend-toolkit/latest/python/site-packages:/usr/local/Ascend/ascend-toolkit/latest/opp/built-in/op_impl/ai_core/tbe:/usr/local/Ascend/ascend-toolkit/latest/opp/op_impl/built-in/ai_core/tbe
 
+
+
 PROJ_PATH='.'
 exp_name='seed_x_detokenizer'
 OUTPUT_PATH=~/SEED-X/train_output/${exp_name}
@@ -10,9 +12,9 @@ OUTPUT_PATH=~/SEED-X/train_output/${exp_name}
 mkdir -p $OUTPUT_PATH
 
 export PYTHONPATH=$PROJ_PATH/proj/peft/src:$PYTHONPATH
-
+wandb login 69672540eb30feaa4b6f38aa2b2aca504e0224ce
 #torchrun --nproc_per_node=$HOST_GPU_NUM --nnodes=$HOST_NUM --master_addr=$CHIEF_IP --master_port=20008 --node_rank=$INDEX \
-torchrun --nproc_per_node=8 \
+torchrun --nproc_per_node=1 \
     ${PROJ_PATH}/src/train/train_detokenizer.py \
     --image_transform ${PROJ_PATH}/configs/processer/qwen_448_transform.yaml \
     --tokenizer ${PROJ_PATH}/configs/tokenizer/clm_llama_tokenizer_224loc_anyres.yaml \
@@ -29,7 +31,7 @@ torchrun --nproc_per_node=8 \
     --adam_beta2 0.98 \
     --adam_epsilon 1e-6 \
     --gradient_accumulation_steps 2 \
-    --mixed_precision bf16 \
+    --mixed_precision fp16 \
     --num_train_epochs 10 \
     --max_steps 20000 \
     --save_steps 1000 \
